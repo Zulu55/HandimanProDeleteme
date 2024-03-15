95e2d98ffc95bcf6906c47ccb91cc7fea457ed98
Juan Zuluaga <juan.zuluaga@cscgfm.com>
2024-03-14 20:29:48 -0500

    Add products to context

---
+++ HandimanProDeleteme/HandimanProDeleteme.Backend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using HandimanProDeleteme.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandimanProDeleteme.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
